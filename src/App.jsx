203035113226d07fad58c3148879b62729a3925f
Ritika Garg <g.ritika@wustl.edu>
2024-11-07 16:34:59 -0600

    squares can alternate between X's and O's now

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,18 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    nextSquares[i] = 'X';
+    setXIsNext(!xIsNext);
   }
 
   return (
